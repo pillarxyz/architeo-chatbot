92f3332a829c77ed9d1739f9bfd9e76eb4cc9ed5
HamBam <62408114+HamzaBamohammed@users.noreply.github.com>
2022-09-13 22:06:49 +0100

    updates nlu and domain files

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,32 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+import arrow 
+import dateparser
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+
+name_db = {
+'Hamza Ba-mohammed',
+'Ayman Lafaz',
+'Hajar El Idrissi',
+'Mohammed Bendahou',
+'Amine Sebti',
+'Ilham Taheri'
+}
+
+class ActionGetFullName(Action):
+
+    def name(self) -> Text:
+        return "action_get_full_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
