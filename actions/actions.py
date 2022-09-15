76ca87f7b332439b2c4055e78c41b496ee72bef0
pillarxyz <ayman.lafaz@um5r.ac.ma>
2022-09-15 01:05:48 +0000

    extract data from user

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,34 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
+from typing import Any, Text, Dict, List, Union
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 #
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+class actions_save_type(Action):
+        def name(self) -> Text:
+            return "actions_save_type"
+    
+        def run(self, dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+            type = next(tracker.get_latest_entity_values("type"), None)
+            dispatcher.utter_message(response="utter_user_intent", type=type)
+            return [SlotSet("type", type)]
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
+class actions_save_name(Action):
+        def name(self) -> Text:
+            return "actions_save_name"
+#    
+        def run(self, dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+            PERSON = next(tracker.get_latest_entity_values("PERSON"), None)
+            if PERSON is not None:
+                PERSON = PERSON.replace("Je suis", "")
+                PERSON = PERSON.replace("Je m'appelle", "")
+                PERSON = PERSON.strip()
+            dispatcher.utter_message(response="utter_personal_data", PERSON=PERSON)
+            return [SlotSet("name", PERSON)]
